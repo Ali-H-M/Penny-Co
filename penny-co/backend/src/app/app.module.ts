4c2b5ca85f74d3d315c96c5335d764c60a29f8d9
Ali Madan <ali-madan@outlook.sa>
2025-08-05 20:06:01 +0300

    Adding secure MongoDB connection using .env file

--- penny-co/backend/src/app/app.module.ts
+++ penny-co/backend/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; // used for .env
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // used to load .env
+    MongooseModule.forRoot(process.env.MONGO_URI), // Connection to MongoDB
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
