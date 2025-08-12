4e428dcd1400c45f5b8e229d464955d127c991ae
alimadan <ali-madan@outlook.sa>
2025-08-12 19:26:32 +0300

    Adding CORS to better link backend/frontend ports

--- penny-co/backend/src/main.ts
+++ penny-co/backend/src/main.ts
@@ -12,6 +12,9 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+    app.enableCors({
+    origin: 'http://localhost:4200', // frontend dev server
+  });
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
